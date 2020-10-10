1dfb631f1f35d5a3108d4d768d014fd5f3f37f56
hamishbrindle <hamishbrindle@gmail.com>
2020-10-10 16:12:46 -0700

    Scaffolding and cleaning up assets

---
+++ src/components/Storefront/Navigation/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="navbar__wrapper">
+    <div class="navbar">
+      &nbsp;
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component({
+  name: 'Navbar',
+  components: {},
+})
+export default class Navbar extends Vue {
+
+}
+</script>
+
+<style lang="scss">
+// .navbar {}
+</style>
